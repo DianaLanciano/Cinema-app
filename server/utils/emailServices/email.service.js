cd3b1029e75552723926c3756e89956195fdf70b
Diana Lanciano <dianalanciano@Dianas-Laptop.local>
2024-11-10 07:39:30 +0200

    mail service is ready

--- server/utils/emailServices/email.service.js
+++ server/utils/emailServices/email.service.js
@@ -4,7 +4,9 @@ const sendNotification = async (email, newMovie) => {
 
   // Parse newMovie back to an object
   const movieData = JSON.parse(newMovie);
-
+  const actorList = movieData.actors.map(actor => `- ${actor}`).join('\n');
+  const image="https://images.moviesanywhere.com/d6c0f192f5ddc9ca2f7ab4f1d566c6d1/71dc56d0-0fe7-4297-a4c6-1f67a801e26d.jpg?r=3x1&w=2400";
+  
     const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
         auth: {
@@ -17,8 +19,12 @@ const sendNotification = async (email, newMovie) => {
         from: '"New Movie Update 👻" <funcinema10@gmail.com>', // sender address
         to: email, // receivers
         subject: `New Movie Released: ${movieData.title}`, // Subject line
-        text: `A new movie has been released!\n\n${newMovie.title}`, // Plain text body, // plain text body
-        //, // HTML body with movie details in a structured format
+        html: `
+        <h2>New Movie Released!</h2>
+        <h3>Actors:\n${actorList}</h3>
+        <img src="${image}" alt="${movieData.title} Poster" style="width:800px; height:auto; margin-top:15px;" />
+        <p>Enjoy the show!</p>
+        `, // html body
       });
 
       console.log("Message sent: ", mailOptions.messageId);
